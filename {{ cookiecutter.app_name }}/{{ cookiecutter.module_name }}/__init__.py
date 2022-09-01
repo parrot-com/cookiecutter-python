355e2a5982a8ce75e8c15191198ad6a60cb65be9
Jakub Hrabec <xhrabec@gmail.com>
2022-09-01 16:59:22 +0200

    handle dash in app name

---
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/__init__.py
@@ -0,0 +1,3 @@
+from {{ cookiecutter.module_name }}.logging import configure_logging
+
+configure_logging()
--- {{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/main.py
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/main.py
@@ -2,9 +2,9 @@ import time
 
 import prometheus_client
 
-from {{ cookiecutter.app_name }}.config import settings
-from {{ cookiecutter.app_name }}.logging import get_logger
-from {{ cookiecutter.app_name }}.metrics import TICKS
+from {{ cookiecutter.module_name }}.config import settings
+from {{ cookiecutter.module_name }}.logging import get_logger
+from {{ cookiecutter.module_name }}.metrics import TICKS
 
 logger = get_logger(__name__)
 
