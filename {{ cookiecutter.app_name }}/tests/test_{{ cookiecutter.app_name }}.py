62b3e78790f3cd03ecc3b90e095d979378d417ea
Jakub Hrabec <xhrabec@gmail.com>
2022-09-07 08:40:37 +0200

    update some app_name references

--- {{ cookiecutter.app_name }}/tests/test_{{ cookiecutter.app_name }}.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from {{ cookiecutter.app_name }}.config import settings
-
-
-# example: this is how you would override settings when running tests
-@pytest.mark.override_settings.with_args(app_env="production")
-def test_it_overrides_env():
-    assert settings.app_env == "production"
