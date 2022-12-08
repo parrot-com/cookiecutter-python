4a2bd5439acc982c5a7fe7f53a0fcc15225cd8f9
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2022-12-09 00:23:14 +0100

    use module_name

--- {{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/metrics.py
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.module_name }}/metrics.py
@@ -1,5 +1,5 @@
 from prometheus_client import Counter
 
-PREFIX = "{{ cookiecutter.app_name }}"
+PREFIX = "{{ cookiecutter.module_name }}"
 
 TICKS = Counter(f"{PREFIX}_ticks_total", "Number of ticks")
